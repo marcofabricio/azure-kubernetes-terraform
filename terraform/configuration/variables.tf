ba565f0b62fb3641505f33f6e82009977be7ac56
Marco Cristofolini <mf.cristofolini@gmail.com>
2020-03-10 00:45:36 -0300

    Update variables.tf

--- terraform/configuration/variables.tf
+++ terraform/configuration/variables.tf
@@ -1,12 +1,6 @@
-variable client_id {
-    default = "171d5191-7b77-49db-a8ff-96d4fa1ff675"
-}
-variable client_secret {
-    default = "ccf60696-5e19-488e-88ca-27d746c4c1a3"
-}
-variable ssh_public_key {
-    default = "id_rsa.pub"
-}
+variable client_id {}
+variable client_secret {}
+variable ssh_public_key {}
 
 variable environment {
     default = "dev"
